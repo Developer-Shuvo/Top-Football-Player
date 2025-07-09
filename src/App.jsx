e2b046fb6147b2db20f2d7e65157eb47ff1a1976
SHUVO <im.computer.engineeer@gamil.com>
2025-07-09 09:29:52 +0600

    Available Players section ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Cover from "./Components/Cover/Cover";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <>
-    <div className="flex  md:w-[1320px] mx-auto   ">
+    <div className="flex flex-col  md:w-[1320px] mx-auto   ">
   <Header></Header>
+  <Main></Main>
   {/* <Cover></Cover> */}
 
     </div>
